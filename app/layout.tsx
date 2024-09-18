82299d6f0f8a05a4dbca2294be5459ddbcee0c32
Syahdan Hayyan Alawy <141197481+alawyyahdan@users.noreply.github.com>
2024-09-18 22:32:51 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <ClickToEnter>
             <VolumeControl />
+            <ClickToEnter>
             <ChadGPT />
             <div className="relative flex flex-col h-screen">
               <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
