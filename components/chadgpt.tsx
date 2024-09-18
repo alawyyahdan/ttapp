54ddcacf9df40cdc927438a281bd1620f70980b5
Syahdan Hayyan Alawy <141197481+alawyyahdan@users.noreply.github.com>
2024-09-18 22:58:54 +0700

    Update chadgpt.tsx

--- components/chadgpt.tsx
+++ components/chadgpt.tsx
@@ -13,7 +13,7 @@ const ChadGPT: React.FC<ChadGPTProps> = ({ className }) => {
   };
 
   return (
-    <div className={className}>
+    <div className={`absolute top-4 left-4 z-50 ${className}`}>
       <Button isIconOnly aria-label="Redirect" onClick={handleRedirect}>
         <RedirectIcon />
       </Button>
