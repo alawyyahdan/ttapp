3d29a09494c7482d9b31fe5ca6429acdd4dcf7a7
Syahdan Hayyan Alawy <141197481+alawyyahdan@users.noreply.github.com>
2024-09-19 14:39:53 +0700

    Update canva.tsx

--- components/canva.tsx
+++ components/canva.tsx
@@ -28,8 +28,6 @@ const RedirectIcon = () => (
     stroke="currentColor"
     strokeWidth={1.5}
     viewBox="0 0 24 24"
-    width="30"  // Ganti dengan ukuran yang diinginkan
-    height="30"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
