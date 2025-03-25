ebbc0da9957cc57e32fce44b64e35ab25e40e76d
Syahdan Hayyan Alawy <141197481+alawyyahdan@users.noreply.github.com>
2025-03-26 01:08:57 +0700

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -3,5 +3,5 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "adan",
   description: "What do you desire?",
-  ogImage: "https://sayato.lol/thumbnail.jpg",
+  ogImage: "https://adan.lat/thumbnail.jpg",
 };
